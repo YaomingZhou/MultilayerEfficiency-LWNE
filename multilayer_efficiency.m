e2c44417c679765ef6ab22300865bc4f503bdbc6
Youngmegit <88269710+Youngmegit@users.noreply.github.com>
2021-08-01 20:22:54 +0800

    Update multilayer_efficiency.m

--- multilayer_efficiency.m
+++ multilayer_efficiency.m
@@ -1,7 +1,7 @@
-function effi=multilayer_efficiency(so,do,N,weight)
+function effi=multilayer_efficiency(so,do,N,weight)%so-source,do-destination,N-number of nodes,weight-number of weekly flights on node pair (so,do)
 effi=[];
 NumofLink=0;
-while max(weight)>0
+while max(weight)>0  
     effi_0=0;
     so_sub=[];do_sub=[];
     for i=1:length(weight)
@@ -11,14 +11,14 @@ while max(weight)>0
             weight(i)=weight(i)-1;
         end
     end
-    if length(so_sub)==NumofLink
+    if length(so_sub)==NumofLink % if the current subnetwork is the same as the previous one, directly use the same unweighted efficiency
         effi_0=effi(end);
     else
         G_sub=graph(so_sub,do_sub);
         distanceMatrix=distances(G_sub);
         n=size(distanceMatrix,1);
         distance=distanceMatrix(:);
-        distance(1:n+1:n*n)=[];
+        distance(1:n+1:n*n)=[];  % delete the 0 value on the diagonal
         effi_0=effi_0+sum(1./distance);
     end
     NumofLink=length(so_sub);
